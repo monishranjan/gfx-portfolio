98859a598f811e997c2123e0dd37bea00dd3ddfb
Monish Ranjan <monishranjan9@gmail.com>
2022-05-07 19:37:41 +0530

    Uploading the website files..

---
+++ assets/js/main.js
@@ -0,0 +1,92 @@
+/*=============== SHOW SIDEBAR ===============*/
+
+
+/*===== SIDEBAR SHOW =====*/
+/* Validate If Constant Exists */
+
+
+/*===== SIDEBAR HIDDEN =====*/
+/* Validate If Constant Exists */
+
+
+/*=============== SKILLS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContent = document.querySelectorAll('[data-content]')
+
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            const target = document.querySelector(tab.dataset.target)
+
+            tabContent.forEach(tabContents => {
+                tabContents.classList.remove("skills__active")
+            })
+
+            target.classList.add('skills__active')
+
+
+            tabs.forEach(tab => {
+                tab.classList.remove("skills__active")
+            })
+
+            tab.classList.add('skills__active')
+        })
+    })
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+
+
+/*===== Link Active Work =====*/
+
+
+/*===== Work Popup =====*/
+
+
+/*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services__modal'),
+    modelBtns = document.querySelectorAll('.services__button'),
+    modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modelBtns.forEach((modelBtn, i) => {
+    modelBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener("click", () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove("active-modal")
+        })
+    })
+})
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+
+/*=============== INPUT ANIMATION ===============*/
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if(this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+})
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/
